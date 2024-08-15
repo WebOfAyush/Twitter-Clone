c2d8923ef03763630d6775997a98cd4db3bc109f
Aayush Bairagi <157678802+WebOfAyush@users.noreply.github.com>
2024-08-15 12:02:25 +0530

    Prepared server.js to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
+import path from "path"
 import {v2 as cloudinary} from "cloudinary"
-import cors from "cors"
+
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -18,19 +19,13 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET,
   });
 const PORT = process.env.PORT || 3000;
-
+const __dirname = path.resolve()
 
 app.use(express.json({limit:"5mb"}));
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
-app.use(
-  cors({
-    origin: "https://x-clone-webofayush.vercel.app",
-    methods: "GET,POST,PUT,DELETE,OPTIONS",
-    allowedHeaders: "Content-Type,Authorization",
-    credentials: true,
-  })
-);
+
+
 
 
 
@@ -39,6 +34,13 @@ app.use("/api/user", userRoutes)
 app.use("/api/post", postRoutes)
 app.use("/api/notifications", notificationRoutes)
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT,  ()=>{
      connectMongoDB();
