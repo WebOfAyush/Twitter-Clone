ca8be57c9f4329de5d511050648e77e38f0016e2
Aayush Bairagi <157678802+WebOfAyush@users.noreply.github.com>
2024-08-15 11:37:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser())
 app.use(
   cors({
-    // origin: "https://todobuddy.vercel.app",
-    origin: "http://localhost:5000",
+    origin: "https://x-clone-webofayush.vercel.app",
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type,Authorization",
     credentials: true,
